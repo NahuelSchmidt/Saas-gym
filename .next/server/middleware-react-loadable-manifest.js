8a9b3c83bb97ac5ee03545f25510f8e8103d5b86
Nahuel Schmidt <nahuel-schmidt@live.com.ar>
2026-06-05 00:01:39 -0300

    Remove .next from repo, update gitignore

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app\\\\dashboard\\\\products\\\\ProductsClient.tsx -> ./actions\":{\"id\":\"app\\\\dashboard\\\\products\\\\ProductsClient.tsx -> ./actions\",\"files\":[]}}"
